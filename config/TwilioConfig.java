f5296f483f06e562596d9426d587b4f99d0f656c
Zain Mustafa <zainmustafa205@gmail.com>
2026-02-14 11:50:10 +0500

    Add files via upload

---
+++ config/TwilioConfig.java
@@ -0,0 +1,23 @@
+package com.example.authsystem.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+
+import com.twilio.Twilio;
+
+import jakarta.annotation.PostConstruct;
+
+@Configuration
+public class TwilioConfig {
+
+    @Value("${twilio.account-sid}")
+    private String accountSid;
+
+    @Value("${twilio.auth-token}")
+    private String authToken;
+
+    @PostConstruct
+    public void init() {
+        Twilio.init(accountSid, authToken);
+    }
+}
