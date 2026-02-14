b71395e196fb57817e3ea67cfd607010a6f80aad
Zain Mustafa <zainmustafa205@gmail.com>
2026-02-14 11:48:29 +0500

    Add files via upload

---
+++ AuthsystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.authsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthsystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthsystemApplication.class, args);
+	}
+
+}
